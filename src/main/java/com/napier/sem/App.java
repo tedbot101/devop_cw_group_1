e524b4902b97dbfd448190b4149e6aa30044fa2b
40478615 <40478615@live.napier.ac.uk>
2022-03-21 10:38:56 +0630

    db connection setup

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,59 @@
 package com.napier.sem;
 
+import java.sql.*;
+
 public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("GROUP 1 ");
+        try
+        {
+            // load jdbc
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("jdbc not found");
+            System.exit(-1);
+        }
+
+        // connect db
+        Connection con = null;
+        int retries = 100;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting ...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // actual connecting code
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Connected Successfully ");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Connection closed");
+            }
+        }
     }
 }
