43e9f4fe56ae91422dc116da1b80eaa77edaf0aa
40478615 <40478615@live.napier.ac.uk>
2022-03-21 11:11:26 +0630

    java db connection

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,58 +1,68 @@
 package com.napier.sem;
 
+
 import java.sql.*;
 
-public class App
-{
-    public static void main(String[] args)
-    {
-        try
-        {
-            // load jdbc
+public class App {
+
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    public static void main(String[] args) {
+        App a = new App();
+
+        // Connect to database
+        a.connect("db:3306", 30000);
+
+        // Disconnect from database
+        a.disconnect();
+    }
+
+
+    /**
+     * Connect to the MySQL database.
+     * @param conString Use db:3306 for docker and localhost:33060 for local or Integration Tests
+     */
+    public void connect(String conString, int delay) {
+        try {
+            // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("jdbc not found");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
-        // connect db
-        Connection con = null;
-        int retries = 100;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting ...");
-            try
-            {
+        int retries = 10;
+        for (int i = 0; i < retries; ++i) {
+            System.out.println("Connecting to database...");
+            try {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
-                // actual connecting code
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Connected Successfully ");
+                Thread.sleep(delay);
+                // Connect to database
+                //Added allowPublicKeyRetrieval=true to get Integration Tests to work. Possibly due to accessing from another class?
+                con = DriverManager.getConnection("jdbc:mysql://" + conString + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
                 break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+            } catch (SQLException sqle) {
+                System.out.println("Failed to connect to database attempt ");
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
-
-        if (con != null)
-        {
-            try
-            {
+    }
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect() {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e)
-            {
-                System.out.println("Connection closed");
+            } catch (Exception e) {
+                System.out.println("Error closing connection to database");
             }
         }
     }
