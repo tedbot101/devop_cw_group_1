ba61b8da0bc5b0a1a48aa44f74eae9b800e29254
tedbot101 <tedliu1999@gmail.com>
2022-03-30 15:01:39 +0630

    integration test

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -1,28 +1,14 @@
 package com.napier.sem;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-
-
-public class AppTest
+class AppTest
 {
-    static App app;
-    @BeforeAll
-    static void init()
-    {
-        app = new App();
-        app.connect("localhost:33060", 30000);
-    }
-
     @Test
     void unitTest()
-    // Test 1: Testing of N number for cities report produced
     {
-        // Insert your test code in here
         assertEquals(5, 5);
     }
 }
