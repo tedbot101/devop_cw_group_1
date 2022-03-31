2ad64e4d5a8f8198d1034083e0b6e98dd156e3e7
40478615 <40478615@live.napier.ac.uk>
2022-03-31 11:05:01 +0630

    AppIntegrationTest for country

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -22,10 +22,48 @@ public class AppIntegrationTest
     }
 
     @Test
-    void testGetCountry() throws SQLException {
-        // Test country record
+    void testCountryPopLargesttoSmallest() throws SQLException {
+
+        // Test CountryPopLargesttoSmallest record
+
+        // testing only one instance
+        Country unit1 = app.getCountryPopLargesttoSmallest().get(0);
+        assertEquals(unit1.getName(),"China");
+        assertEquals(unit1.getContinent(),"Asia");
+        assertEquals(unit1.getRegion(),"Eastern Asia");
+        assertEquals(unit1.getCapital(),"1891");
+        assertEquals(unit1.getPopulation(),1.277558016E9 );
+
+    }
+
+    @Test
+    void testgetCountryPopbyContinent() throws SQLException {
+
+        // Test CountryPopLargesttoSmallest record
+
+        // testing only one instance
+        Country unit2 = app.getCountryPopbyContinent("Asia").get(0);
+        assertEquals(unit2.getName(),"China");
+        assertEquals(unit2.getContinent(),"Asia");
+        assertEquals(unit2.getRegion(),"Eastern Asia");
+        assertEquals(unit2.getCapital(),"1891");
+        assertEquals(unit2.getPopulation(),1.277558016E9 );
 
 
     }
 
+    @Test
+    void testgetCountryPopbyRegion() throws SQLException {
+
+        // Test CountryPopLargesttoSmallest record
+
+        // testing only one instance
+        Country unit3 = app.getCountryPopbyRegion("Caribbean").get(0);
+        assertEquals(unit3.getName(),"Cuba");
+        assertEquals(unit3.getContinent(),"North America");
+        assertEquals(unit3.getRegion(),"Caribbean");
+        assertEquals(unit3.getCapital(),"2413");
+        assertEquals(unit3.getPopulation(),1.1201E7 );
+
+    }
 }
